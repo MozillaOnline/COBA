0e68ea16746900e76c57207ee5228061b3d15fc3
Med0paW <medopaw@gmail.com>
2014-02-18 13:59:27 +0800

    Update offcial rule url

--- extension/defaults/preferences/coba.js
+++ extension/defaults/preferences/coba.js
@@ -13,7 +13,7 @@ pref("extensions.coba.compatMode", "ie7mode");
 
 
 pref("extensions.coba.official.filter", true);
-pref("extensions.coba.official.updateurl", "http://i.g-fox.cn/notification/coba_official.json");
+pref("extensions.coba.official.updateurl", "http://i.g-fox.cn/notification/coba_official_v2.json");
 pref("extensions.coba.official.filter.update", true);
 pref("extensions.coba.official.allwaysupdate", true);
 pref("extensions.coba.official.filterlist", "*.icbc.com.cn* *.abchina.com* *.95599.cn* *.ecitic.com* *.cebbank.com* *.bankcomm.com* *.95559.com.cn* *.hxb.com.cn* *.psbc.com* *.bankofbeijing.com.cn* https://ebspay.boc.cn* *.srcb.com* *.bankofshanghai.com* *.hzbank.com.cn* *.bjrcb.com* *.fudian-bank.com* *.wzcb.com.cn* *.cbhb.com.cn* *.njcb.com.cn* *.chinastock.com.cn* *member.chinaacc.com//selectcourse/confirmSelect.shtm?* *toefl.etest.net.cn/*/SelectFeeType*");
